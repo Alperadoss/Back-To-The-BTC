90b1340dd0e634098eb5946bf8f39258147131d7
Alper Yazagan <alperyazagan@gmail.com>
2023-04-02 22:03:40 +0300

    upgrade list added

--- src/components/Upgrades.jsx
+++ src/components/Upgrades.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 
 export default function Upgrades() {
-  return (
-    <div className="upgrades">
-      <h1>Mining Upgrades</h1>
-      <div>
-        <h3>2nd hand PC</h3>
-        <p>$100 - A second hand computer (xx mining power)</p>
-      </div>
-      <div>
-        <h3>Cool gaming PC</h3>
-        <p>$200 - PC with powerful graphic card (xy mining power)</p>
-      </div>
-      <div>
-        <h3>Cooler gaming PC</h3>
-        <p>$300 - gaming PC with best graphic card (xz mining power)</p>
-      </div>
-      <div>
-        <h3>Mining Rig S6 Pro</h3>
-        <p>$1200 - mining system with 6 graphic cards (zz mining power)</p>
-      </div>
-    </div>
-  );
+  const upgradeItems = [
+    {
+      name: "2nd hand PC",
+      info: "$100 - A second hand computer (0.01x mining power)",
+      price: 100,
+      power: 0.01,
+    },
+    {
+      name: "Cool gaming PC",
+      info: "$200 - PC with powerful graphic card (0.05x mining power)",
+      price: 200,
+      power: 0.05,
+    },
+    {
+      name: "Gaming PC with RGB LEDs",
+      info: "$300 - RGB LEDs add extra GPU power (0.07x mining power)",
+      price: 300,
+      power: 0.07,
+    },
+    {
+      name: "Mining Rig S6 Pro",
+      info: "$1000 - Mining Rig (1x mining power)",
+      price: 1000,
+      power: 1,
+    },
+    {
+      name: "S6 Pro Max",
+      info: "$1200 - same with S6 Pro, marketed for fanboys (1x mining power)",
+      price: 1200,
+      power: 1,
+    },
+    {
+      name: "Crypto Trading Website",
+      info: "manipulate the market, earn more! (5x mining power)",
+      price: 100000,
+      power: 5,
+    },
+    {
+      name: "MVidia",
+      info: "Now all factory is producing GPUs just for you!  (100x mining power)",
+      price: 10000000,
+      power: 100,
+    },
+  ];
 }
