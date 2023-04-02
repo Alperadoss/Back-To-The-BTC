7512cf4f475ae9371ccea0bfb8ddb246c943244c
Alper Yazagan <alperyazagan@gmail.com>
2023-04-02 15:24:45 +0300

    decimals must be arrenged later

--- src/components/StockMarket.jsx
+++ src/components/StockMarket.jsx
@@ -3,14 +3,34 @@ import React from "react";
 export default function StockMarket(props) {
   function handleBTCSubmit(event) {
     event.preventDefault();
-    props.setUsdbalance(event.target[0].value);
-    //let newUsd = props.usdbalance;
+
+    let formAmount = event.target[0].value;
+    let earlierUSD = props.usdbalance;
+    let earlierBTC = props.btcbalance;
+    if (props.btcbalance >= formAmount) {
+      props.setUsdbalance(earlierUSD + props.btcUsdRatio * formAmount);
+      props.setBtcbalance(earlierBTC - formAmount);
+    }
+    event.target[0].value = "";
+  }
+
+  function handleUSDSubmit(event) {
+    event.preventDefault();
+
+    let formAmount = event.target[0].value;
+    let earlierUSD = props.usdbalance;
+    let earlierBTC = props.btcbalance;
+    if (props.usdbalance >= formAmount) {
+      props.setBtcbalance(earlierBTC + formAmount / props.btcUsdRatio);
+      props.setUsdbalance(earlierUSD - formAmount);
+    }
+    event.target[0].value = "";
   }
 
   return (
     <div className="stockmarket">
       <h1>Stock Market</h1>
-      <h3>Rate: 1230 USD/BTC</h3>
+      <h3>Rate: {props.btcUsdRatio} USD/BTC</h3>
 
       <div className="buysell">
         <form onSubmit={handleBTCSubmit}>
@@ -21,7 +41,7 @@ export default function StockMarket(props) {
       <br></br>
 
       <div className="buysell">
-        <form onSubmit={handleBTCSubmit}>
+        <form onSubmit={handleUSDSubmit}>
           USD: <input type="number" name="usdamount"></input>
           <button type="submit">SELL USD</button>
         </form>
